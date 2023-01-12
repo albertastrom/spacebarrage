91566de4f95565fd47fa178430240d80ed9e65d8
mcheney4 <mason.cheney@comcast.net>
2023-01-12 12:03:14 -0500

    added file to move shield barrier and rotate

---
+++ Assets/MoveShield.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShield : MonoBehaviour
+{
+    public float shieldX;
+    public float shieldY;
+    public float shieldDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shieldX = 0;
+        shieldY = 0;
+        shieldDirection = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if left arrow key was pressed
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // move shield left if x greater than -5
+            if (shieldX > -5)
+            {
+                shieldX -= 0.01;
+            }
+            
+        }
+        
+        // check if right arrow key was pressed
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // move shield right if x less than 5
+            if (shieldX < 5)
+            {
+                shieldX += 0.01;
+            }
+            
+        }
+
+        //check if up arrow was pressed
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // move shield up if y is less than 5
+            if (shieldY < 5)
+            {
+                shieldY += 0.01;
+            }
+            
+        }
+
+        //check if down arrow is pressed
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // move shield down if y greater than -5
+            if (shieldY > -5)
+            {
+                shieldY -= 0.01;
+            }
+            
+        }
+
+        //check to see if A is pressed
+        else if (Input.GetKey(KeyCode.A))
+        {
+            //rotate spaceship left 
+            shieldDirection -= 1;
+        }
+
+        //check to see if D is pressed
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //rotate spaceship right
+            shieldDirection += 1;
+        }
+
+        else if (Input.GetKey(KKeyCode.R))
+        {
+            shieldDirection = 0;
+            shieldX = 0;
+            shieldY = 0;
+        }
+    }
+}
