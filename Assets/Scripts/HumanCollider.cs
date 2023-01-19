50e5bcfb26c38104d61b8ceb77a4a09ec76db58e
mcheney4 <mason.cheney@comcast.net>
2023-01-19 12:32:33 -0500

    moved all scripts into scripts folder

---
+++ Assets/Scripts/HumanCollider.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanCollider : MonoBehaviour
+{
+    public int health;
+
+    public GameMaster gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDead();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Alien_Missile")
+        {
+            Destroy(col.gameObject);
+            health -= 1;
+        }
+        Debug.Log("OnCollisionEnter2D");
+    }
+
+    void isDead()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            // destroy all missiles
+            GameObject[] missiles = GameObject.FindGameObjectsWithTag("Human_Missile");
+            foreach (GameObject missile in missiles)
+            {
+                Destroy(missile);
+            }
+            // stop game
+            gm.isRunning = false;
+        }
+    }
+}
