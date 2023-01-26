fd0049cf93232a1ed18a21b09d568e47b19dcf5d
mcheney4 <mason.cheney@comcast.net>
2023-01-26 02:21:27 -0500

    fixed explosion animation prefab

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Animator>().Play("Explosion");
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
 }
